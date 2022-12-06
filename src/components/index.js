0dd0485f1006cf3af1da5bb41279c546c96cc432
lucas.singh <lucas.singh@bizitglobal.com>
2022-12-06 15:38:56 -0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import Cta from "./Cta";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    Cta,
+    Stats,
+    Footer,
+    Testimonials,
+    GetStarted,
+    Hero,
+};
